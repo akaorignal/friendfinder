fa669c210d85bc904a3826fb475a4068f81e3590
akaorignal <torir.adams@gmail.com>
2017-10-22 12:46:06 -0700

    aster

---
+++ app/data/friends.js
@@ -0,0 +1,45 @@
+// ===============================================================================
+// DATA
+// FRIENDS HERE 
+// ===============================================================================
+
+var friends = [ {
+	"name" : "Derick",
+	"photo" : "https://images.pexels.com/photos/428333/pexels-photo-428333.jpeg?w=940&h=650&auto=compress&cs=tinysrgb",
+	"scores" : [5,5,2,3,1,5,4,1,3,5]
+}, {
+	"name" : "Simon",
+	"photo" : "https://pbs.twimg.com/media/BcINeMVCIAABeWd.jpg",
+	"scores" : [1,5,1,1,1,1,1,1,5,5]
+}, {
+	"name" : "James",
+	"photo" : "https://images.pexels.com/photos/450271/pexels-photo-450271.jpeg?w=940&h=650&auto=compress&cs=tinysrgb",
+	"scores" : [3,3,2,3,2,5,1,3,3,6]
+}, {
+	"name" : "Matthew",
+	"photo" : "https://images.pexels.com/photos/462680/pexels-photo-462680.jpeg?w=940&h=650&auto=compress&cs=tinysrgb",
+	"scores" : [1,5,1,1,1,1,5,1,5,5]
+}, {
+	"name" : "Tasha",
+	"photo" : "https://images.pexels.com/photos/157907/smile-color-laugh-black-157907.jpeg?w=940&h=650&auto=compress&cs=tinysrgb",
+	"scores" : [2,5,1,1,1,1,1,1,5,5]
+}, {
+	"name" : "Xuesie",
+	"photo" : "https://images.pexels.com/photos/157920/woman-face-curly-hair-157920.jpeg?w=940&h=650&auto=compress&cs=tinysrgb",
+	"scores" : [1,5,1,1,1,1,1,1,5,5]
+}, {
+	"name" : "Molly",
+	"photo" : "https://images.pexels.com/photos/218721/pexels-photo-218721.jpeg?w=940&h=650&auto=compress&cs=tinysrgbhttps://pbs.twimg.com/media/BcINeMVCIAABeWd.jpg",
+	"scores" : [4,5,1,1,1,1,1,1,5,5]
+}, {
+	"name" : "Christa",
+	"photo" : "https://images.pexels.com/photos/372042/pexels-photo-372042.jpeg?w=940&h=650&auto=compress&cs=tinysrgb",
+	"scores" : [1,5,1,1,1,1,1,1,5,5]
+}, {
+	"name" : "Simon",
+	"photo" : "https://pbs.twimg.com/media/BcINeMVCIAABeWd.jpg",
+	"scores" : [5,5,1,1,1,1,1,1,5,5]
+}];
+
+
+module.exports = friends;
