c1e6905968d2bfdcc095ac0c3ac3a95d66253f95
akaorignal <torir.adams@gmail.com>
2017-10-22 22:35:33 -0700

    aster

--- app/routing/htmlr.js
+++ app/routing/htmlr.js
@@ -3,29 +3,24 @@
 // Series of npm packages that we will use to give our server useful functionality
 // ==============================================================================
 var express = require('express');
-var path = require("path");
+
 // ==============================================================================
 // EXPRESS CONFIGURATION
 // This sets up the basic properties for our express server
-// ==============================================================================
-// Tells node that we are creating an "express" server
-var app = express();
-// Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 8080;
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-// ================================================================================
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
-// =============================================================================
-// LISTENER
-// The below code effectively "starts" our server
-// =============================================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+
+//  'get' route with the url /survey.
+// default 'use' route  home.html
+// dependencies
+var path = require('path');
+
+module.exports = function(app){ // app=express
+// survey page
+app.get('/survey', function(req, res){
+res.sendFile(path.join(__dirname + '/../public/survey.html'));
 });
+// home page
+app.use(function(req, res){
+  res.sendFile(path.join(__dirname + '/../public/home.html'));
+});
+
+};
